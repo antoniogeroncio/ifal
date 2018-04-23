17a8d0becf28da4eef1e944c59954c3157ad8096
Antonio Geroncio <antonio.santos.mcz@gmail.com>
2018-04-22 22:07:22 -0300

    projeto sem refatoramento.

---
+++ fabrica_musica_old/src/Animal.java
@@ -0,0 +1,17 @@
+
+public class Animal {
+	private String descricao;
+	
+	public Animal(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+}
